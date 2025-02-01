617b7d9db16f4068fc969e4ca05dd813608443c7
K_Minhyeok <61306657+Kim-Min-Hyeok@users.noreply.github.com>
2025-02-01 15:21:36 +0900

    [Feat]: CI_CD_Test

---
+++ CI_CD_TestApp/CI_CD_TestApp/CI_CD_TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_CD_TestAppApp.swift
+//  CI_CD_TestApp
+//
+//  Created by Minhyeok Kim on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CI_CD_TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CI_CD_TestApp/CI_CD_TestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CI_CD_TestApp
+//
+//  Created by Minhyeok Kim on 2/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
